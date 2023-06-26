6296593b0502207bf7e8bab5a7ec69038e756c89
kone mamoudou <konemamoudou84@gmail.com>
2023-06-26 23:43:00 +0200

    ajout auto des table en bd :white_check_mark:

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,45 @@
+import entities.Category;
+import entities.Produit;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
 
 
-
+    private static final Logger LOGGER = LoggerFactory.getLogger("monLogger");
     public static void main(String[] args) {
-      
+        LOGGER.warn(" Log  _____________________________________________________________________________________________________");
 
-        System.out.println("hello  ");
+
+        try(
+                // create entity manager factory
+                EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("banque");
+                //  create entity factory
+                EntityManager entityManager = entityManagerFactory.createEntityManager() ) {
+
+            // start transaction
+            entityManager.getTransaction().begin();
+
+            //create cate
+
+            Category category = new Category();
+            category.setNom("catenew");
+
+            //
+            Produit produit = new Produit();
+            produit.setNom("maison");
+            produit.setCategory(category);
+
+            entityManager.persist(produit);
+            entityManager.persist(category);
+
+
+            entityManager.getTransaction().commit();
+
+        }
 
     }
 }
--- src/main/java/entities/Produit.java
+++ src/main/java/entities/Produit.java
@@ -121,6 +121,10 @@ public class Produit {
     /** * Constructeur */
     public Produit() {}
 
+    public Produit(String nom) {
+        this.nom = nom;
+    }
+
     public Produit(String nom, String grade, Double energie100g, Double graisse100g, Double sucres100g, Double fibres100g, Double proteines100g, Double sel100g, Double vitA100g, Double vitD100g, Double vitE100g, Double vitK100g, Double vitC100g, Double vitB1100g, Double vitB2100g, Double vitPP100g, Double vitB6100g, Double vitB9100g, Double vitB12100g, Double calcium100g, Double magnesium100g, Double iron100g, Double fer100g, Double betaCarotene100g) {
         this.nom = nom;
         this.grade = grade;
